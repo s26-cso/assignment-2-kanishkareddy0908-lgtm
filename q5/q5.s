ef8b6d797158da6c31288183e4dd57d7972fec86
Kanishka Reddy <kanishkareddy0908@gmail.com>
2026-04-14 10:00:03 +0530

    modified q2.s

--- q5/q5.s
+++ q5/q5.s
@@ -20,41 +20,38 @@ main:
     beq x8,x0,exit_err      #if fp==NULL,exit
     addi x10,x8,0           
     addi x11,x0,0           
-    addi x12,x0,2           # SEEK_END
+    addi x12,x0,2           #SEEK_END
     call fseek
-    
     addi x10,x8,0           
     call ftell              
-    addi x9,x10,-1          # x9 = right = size-1
-    addi x18,x0,0           # x18 = left = 0
+    addi x9,x10,-1          #x9=right=size-1
+    addi x18,x0,0           #x18=left= 0
 
 loop:
     bge x18,x9,is_palindrome 
     addi x10,x8,0           
     addi x11,x18,0          
-    addi x12,x0,0           # SEEK_SET
+    addi x12,x0,0           #SEEK_SET
     call fseek
-    addi x10,x2,0           # buffer at stack
+    addi x10,x2,0           #buffer at stack
     addi x11,x0,1           
     addi x12,x0,1           
     addi x13,x8,0           
     call fread              
     addi x10,x8,0           
     addi x11,x9,0           
-    addi x12,x0,0           # SEEK_SET
+    addi x12,x0,0           #SEEK_SET
     call fseek
-    addi x10,x2,1           # buffer at stack+1
+    addi x10,x2,1           #buffer at stack+1
     addi x11,x0,1           
     addi x12,x0,1           
     addi x13,x8,0           
     call fread              
-
-    lbu x5,0(x2)            
-    lbu x6,1(x2)            
+    lb x5,0(x2)            
+    lb x6,1(x2)            
     bne x5,x6,not_palindrome
-
-    addi x18,x18,1          # left++
-    addi x9,x9,-1           # right--
+    addi x18,x18,1          #left++
+    addi x9,x9,-1           #right--
     jal x0,loop             
 
 is_palindrome:
